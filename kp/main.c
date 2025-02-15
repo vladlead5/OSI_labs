3c9c3fd59b549cf96e19211d8adf416a24676484
Vladislav Reznik <137673057+vladlead5@users.noreply.github.com>
2025-02-15 19:23:03 +0300

    Create main.c
    done

---
+++ kp/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "include/tree_manager.h"
+#include "include/rabbitmq_utils.h"
+
+int main()
+{
+    char command[256];
+    initialize_tree();
+    rabbitmq_initialize();
+
+    printf("Distributed System Command Line\n");
+    while (1)
+    {
+        printf("> ");
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+            break;
+        }
+
+        command[strcspn(command, "\n")] = 0;
+
+        if (strncmp(command, "create", 6) == 0)
+        {
+            handle_create_command(command);
+        }
+        else if (strncmp(command, "exec", 4) == 0)
+        {
+            handle_exec_command(command);
+        }
+        else if (strncmp(command, "ping", 4) == 0)
+        {
+            handle_ping_command(command);
+        }
+        else if (strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+        else if (strncmp(command, "kill", 4) == 0)
+        {
+            handle_kill_command(command);
+        }
+        else
+        {
+            printf("Error: Unknown command\n");
+        }
+    }
+
+    cleanup_tree(NULL);
+    rabbitmq_close();
+    return 0;
+}
